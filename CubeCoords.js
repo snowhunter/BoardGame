d99501b7008728a866a38ca1134319ba0bafa05c
Boulasikis Michail <mixboul@gmail.com>
2017-08-18 01:22:37 +0300

    Cube Coordinates class with support methods

---
+++ CubeCoords.js
@@ -0,0 +1,31 @@
+
+function CubeCoords (x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+
+CubeCoords.prototype.getX = function () { return this.x; }
+CubeCoords.prototype.getY = function () { return this.y; }
+CubeCoords.prototype.getZ = function () { return this.z; }
+
+var cube_directions = [
+    new CubeCoords(+1, -1,  0), new CubeCoords(+1,  0, -1), new CubeCoords( 0, +1, -1),
+    new CubeCoords(-1, +1,  0), new CubeCoords(-1,  0, +1), new CubeCoords( 0, -1, +1)
+]
+
+CubeCoords.prototype.add = function (cube1) {
+    var result = new CubeCoords(this.x + cube1.getX(), this.y + cube1.getY(), this.z + cube1.getZ());
+    return result;
+}
+
+function axesToCube(x1, y1) {
+    var x = x1 - ((y1 - (y1 & 1)) / 2);
+    var z = y1;
+    var y = - x - z;
+    return new CubeCoords(x, y, z);
+}
+
+CubeCoords.prototype.cubeToAxes = function () {
+    return [this.x + ((this.z - (this.z & 1)) / 2), this.z];
+}
